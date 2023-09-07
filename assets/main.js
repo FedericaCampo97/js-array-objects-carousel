fde07810878e60cb3a70f6ba69fea4851ba59bee
FedericaCampo97 <federica.campo97@gmail.com>
2023-09-07 16:25:02 +0200

    init project start array object

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,31 @@
 /*Modifichiamo il codice dell'esercizio per renderlo 
 funzionante con un array di oggetti al posto di un array 
 di stringhe.*/
+
+//Creo un array oggetto di immagini
+const photo = [
+   {
+    image: './img/01.webp'
+   },
+   {
+    image: './img/02.webp'
+   },
+   { 
+    image: './img/03.webp'
+   },
+   {
+    image: './img/04.webp'
+   },
+]
+
+let activeSlide = 0;
+
+
+
+
+
+
+
+
+
+
