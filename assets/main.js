f57493d45e2e44e53430091b80b5d71c887fa187
FedericaCampo97 <federica.campo97@gmail.com>
2023-09-07 16:31:38 +0200

    put  images in html with foreach

--- assets/main.js
+++ assets/main.js
@@ -20,6 +20,20 @@ const photo = [
 
 let activeSlide = 0;
 
+// Seleziono gli elementi html
+const sliderImagesEl = document.querySelector('.slider .images')
+const prevEl = document.querySelector('.prev')
+const nextEl = document.querySelector('.next')
+
+//Faccio ciclo foreach
+photo.forEach((element,key) => {
+    const slidePath = element.image;
+    
+    //Creo l'elemtno img e verifico l'active dalla key
+    const slideMarkup = `<img class="${activeSlide === key ? 'active' : '' }" src="${slidePath}" alt="">`
+  
+    sliderImagesEl.insertAdjacentHTML('beforeend', slideMarkup)
+})
 
 
 
